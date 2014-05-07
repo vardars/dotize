ec2ba7303fe1b8564b5a4303a021918c2f3af685
vardars <vardars@users.noreply.github.com>
2014-05-07 10:29:00 +0300

    Array compatibility added

--- dotize.js
+++ dotize.js
@@ -1,11 +1,14 @@
+// https://github.com/vardars/dotize
 var dotize = dotize || {};
-
 dotize.convert = function(jsonobj, prefix) {
     var newobj = {};
     function recurse(o, p) {
         for (var f in o) {
             if (o[f] && typeof o[f] === "object") {
-                newobj = recurse(o[f], (p ? p  + "." : "") + f);
+                if (Array.isArray(o[f]))
+                    newobj[(p ? p  + "." : "") + f] = o[f];
+                else
+                    newobj = recurse(o[f], (p ? p  + "." : "") + f);
             } else {
                 newobj[p + "." + f] = o[f];
             }
