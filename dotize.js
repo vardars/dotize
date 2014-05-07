2400f18321d733472dd270fdef027dd41b7eaca1
vardars <vardars@users.noreply.github.com>
2014-05-07 13:39:28 +0300

    Array item bugfix

--- dotize.js
+++ dotize.js
@@ -2,18 +2,27 @@
 var dotize = dotize || {};
 dotize.convert = function(jsonobj, prefix) {
     var newobj = {};
-    function recurse(o, p) {
+
+    function recurse(o, p, isArrayItem) {
         for (var f in o) {
             if (o[f] && typeof o[f] === "object") {
                 if (Array.isArray(o[f]))
-                    newobj[(p ? p  + "." : "") + f] = o[f];
-                else
-                    newobj = recurse(o[f], (p ? p  + "." : "") + f);
+                    newobj = recurse(o[f], (p ? p + "." : "") + f, true); // array
+                else {
+                    if (isArrayItem)
+                        newobj = recurse(o[f], (p ? p : "") + "[" + f + "]"); // array item object
+                    else
+                        newobj = recurse(o[f], (p ? p + "." : "") + f); // object
+                }
             } else {
-                newobj[p + "." + f] = o[f];
+                if (isArrayItem)
+                    newobj[p + "[" + f + "]"] = o[f]; // array item primitive
+                else
+                    newobj[p + "." + f] = o[f]; // primitive
             }
         }
         return newobj;
     }
+
     return recurse(jsonobj, prefix);
 };
