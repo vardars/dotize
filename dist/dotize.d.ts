e03987024ae170e04ee1eacef3976b104612596b
vardars <vardars@users.noreply.github.com>
2025-04-29 17:56:34 +0000

    mocha version upgrade, .d.ts file fix, bump version 0.4

--- dist/dotize.d.ts
+++ dist/dotize.d.ts
@@ -1,19 +1,26 @@
-export namespace valTypes {
-    const none: string;
-    const primitive: string;
-    const object: string;
-    const array: string;
+export const enum valTypes {
+    none = "NONE",
+    primitive = "PRIM",
+    object = "OBJECT",
+    array = "ARRAY",
 }
-export function getValType(val: any): string;
-export function getPathType(arrPath: any): string[];
-export function isUndefined(obj: any): boolean;
-export function isNumber(f: any): boolean;
-export function isEmptyObj(obj: any): boolean;
+
+export function getValType(val: any): valTypes;
+export function getPathType(arrPath: any[]): string[];
+export function isUndefined(obj: any): obj is undefined;
+export function isNumber(f: any): f is number;
+export function isEmptyObj(obj: object): boolean;
 export function isNotObject(obj: any): boolean;
-export function isEmptyArray(arr: any): boolean;
-export function isNotArray(arr: any): boolean;
-export function removeEmptyArrayItem(arr: any): any;
-export function getFieldName(field: any, prefix: any, isRoot: boolean, isArrayItem: boolean, isArray: boolean): string;
-export function startsWith(val: any, valToSearch: any): boolean;
-export function convert(obj: any, prefix: any): any;
-export function backward(obj: any, prefix: any): any;
+export function isEmptyArray(arr: any[]): boolean;
+export function isNotArray(arr: any): arr is Array<never>;
+export function removeEmptyArrayItem(arr: any[]): any[];
+export function getFieldName(
+    field: string,
+    prefix: string,
+    isRoot: boolean,
+    isArrayItem: boolean,
+    isArray: boolean
+): string;
+export function startsWith(val: string, valToSearch: string): boolean;
+export function convert(obj: object, prefix?: string): object;
+export function backward(obj: object, prefix?: string): object;
--- src/dotize.js
+++ src/dotize.js
@@ -2,7 +2,7 @@
 // url: https://github.com/vardars/dotize
 // author: vardars
 
-var dotize = {
+const dotize = {
     valTypes: {
         none: "NONE",
         primitive: "PRIM",
@@ -208,6 +208,4 @@ var dotize = {
     }
 }
 
-if (typeof module != "undefined") {
-    module.exports = dotize;
-}
+export default dotize;
